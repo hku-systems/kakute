15e3158e3120851011867ca26101a83562273a0c
jianyu <jianyu@hku.hk>
2017-05-03 21:22:22 +0800

    example

--- examples/src/main/scala/edu/hku/cs/dft/examples/TwitterLineageOriginal.scala
+++ examples/src/main/scala/edu/hku/cs/dft/examples/TwitterLineageOriginal.scala
@@ -6,21 +6,16 @@ import org.apache.spark.{SparkConf, SparkContext}
 /**
   * Created by jianyu on 5/3/17.
   */
-object TwitterLineage {
+object TwitterLineageOriginal {
   def main(args: Array[String]): Unit = {
     val conf = new SparkConf()
     val twitterfile = args(0)
 
     val sc = new SparkContext(conf)
 
-    var file = sc.textFile(twitterfile, 40)
+    var file = sc.textFile(twitterfile, 10)
 
-    val taint_file = file.zipWithUniqueId().taint(t => {
-      (t._2, -1)
-    })
-      .map(_._1)
-
-    val to = taint_file.map(
+    val to = file.map(
       line => {
         val list = line.split(",")
         if(list.size > 4){
@@ -37,7 +32,6 @@ object TwitterLineage {
 
     val file1 = to.groupByKey()
 
-
     val fm = file1.flatMap(line =>
       line._2.flatMap(l => l.trim().split(" ")))
     val pair = fm.map{word =>
@@ -45,12 +39,9 @@ object TwitterLineage {
     }
 
     val count = pair.reduceByKey(_ + _)
-        .zipWithTaint().map(t => {
-      val taintTuple = t._2.asInstanceOf[(CombinedTaint[_], CombinedTaint[_])]
-      (t._1, taintTuple._2.iterator.toArray)
-    })
+
     count.collect().foreach(t => {
-      println(t._1 + " " + t._2.length)
+      println(t._1 + " " + t._2)
     }
     )
 
