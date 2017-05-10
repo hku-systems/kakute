c351a18bea9b2cbf8bce4aff0f7c97e319296023
jianyu <jianyu@hku.hk>
2017-05-10 13:48:08 +0800

    examples

--- examples/src/main/scala/edu/hku/cs/dft/examples/provenance/ProvenanceGraphExample.scala
+++ examples/src/main/scala/edu/hku/cs/dft/examples/provenance/ProvenanceGraphExample.scala
@@ -1,5 +1,6 @@
 package edu.hku.cs.dft.examples.provenance
 
+import edu.hku.cs.dft.tracker.CombinedTaint
 import org.apache.spark.sql.SparkSession
 
 /**
@@ -34,16 +35,41 @@ object ProvenanceGraphExample {
     val nodes = edges.flatMap(edge => Array(edge._1, edge._2)).distinct()
 
     // Init the node with its own id
-    var label_node = nodes.map(t => (t, t))
+    var label_node = nodes.map(t => (t, (t, 0)))
 
     for (i <- 1 to 10) {
       // group nodes with the same id
-      val new_label = label_node.join(edges).map(t => (t._2._2, t._2._1)).union(label_node)
-      label_node = new_label.reduceByKey(math.min)
+      var new_label = label_node.join(edges).map(t => (t._2._2, (t._2._1._1, i)))
+      if (trace)
+         new_label = new_label.zipWithTaint()
+        .taint(t => {
+          var taint_set: Set[Long] = Set()
+          val taint_tuple = t._2.asInstanceOf[(_, _)]
+          var node_taint = taint_tuple._2.asInstanceOf[CombinedTaint[_]].iterator.toSet
+          val edge_taint = taint_tuple._1.asInstanceOf[CombinedTaint[_]].iterator.toList
+          node_taint.foreach(t => t match {
+            case s: Set[Long] => taint_set = taint_set ++ s
+            case k: Long => taint_set = taint_set + k
+            case _ => throw new IllegalArgumentException("illegal")
+          })
+          taint_set = taint_set + edge_taint.head.asInstanceOf[Long]
+          ((-1, taint_set), -1)
+        })
+        .map(_._1)
+      val m_label = new_label.union(label_node)
+      label_node = m_label.reduceByKey(math.min)
     }
 
     if (trace)
-      label_node.collectWithTaint().foreach(println)
+      label_node.collectWithTaint().foreach(t => {
+        print(t._1 + " ")
+        val taint_tuple = t._2.asInstanceOf[(_, _)]
+        print(taint_tuple._1 + " ")
+        val maybeList = taint_tuple._2.asInstanceOf[CombinedTaint[_]].iterator.toList
+        if (maybeList.nonEmpty)
+          maybeList.head.asInstanceOf[Set[_]].foreach(m => print(m + " "))
+        println()
+      })
     else
       label_node.collect().foreach(println)
 
---
+++ examples/src/main/scala/edu/hku/cs/dft/examples/provenance/TwitterLineage.scala
@@ -0,0 +1,63 @@
+package edu.hku.cs.dft.examples.provenance
+
+import edu.hku.cs.dft.tracker.CombinedTaint
+import org.apache.spark.{SparkConf, SparkContext}
+
+/**
+  * Created by jianyu on 5/3/17.
+  */
+object TwitterLineage {
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf()
+    val twitterfile = args(0)
+
+    val sc = new SparkContext(conf)
+
+    var file = sc.textFile(twitterfile, 10)
+
+    val trace = args.length > 1 && args(1).equals("true")
+
+    if (trace)
+      file = file.zipWithUniqueId().taint(t => {
+      (t._2, -1)
+    }).map(_._1)
+
+    val to = file.map(
+      line => {
+        val list = line.split(",")
+        if(list.size > 4){
+          val time = list(0).split(" ")(3).split(":")(1)
+          val text = list(3)
+          (time, text)
+        }
+        else{
+          ("15:02:45","  ")
+        }
+      }
+
+    )
+
+    val file1 = to.groupByKey()
+
+
+    val top_words = file1.map(t => {
+      val words = t._2.flatMap(_.trim.split(" "))
+      val sorted_arr = words.map(t => (t, if (t.length > 0) (t.charAt(0).toInt | 1) & 1 else 1))
+        .groupBy(_._1).map(l => (l._1, l._2.map(_._2).reduce(_ + _)))
+        .toArray
+//        .sortBy(_._2)
+      (t._1, sorted_arr)
+    })
+
+    val time_group = top_words.flatMapValues(t => t)
+
+    if (trace)
+      time_group.collectWithTaint().foreach(println)
+    else
+      time_group.collect().foreach(println)
+
+    readLine()
+
+    sc.stop()
+  }
+}
