dda555b018ef4eb0d1af2d092c01eeb9d63ebf5e
jianyu-m <jianyu@hku.hk>
2017-09-23 23:50:27 -0700

    example

--- examples/src/main/scala/edu/hku/cs/dft/examples/APIExample.scala
+++ examples/src/main/scala/edu/hku/cs/dft/examples/APIExample.scala
@@ -16,34 +16,13 @@ object APIExample {
       .config("spark.dft.tracking.mode", "full")
       .getOrCreate()
 
-    val file = if (args.length > 0)
-      args(0)
-    else
-      throw new Exception("please give the input file")
+    val in = spark.sparkContext.parallelize(Seq(("Max", "X-Man", 3),
+                                            ("Max", "Kingsman", 5),
+                                            ("John", "Kingsman", 4)), 2)
 
-    val text = spark.read.textFile(file).rdd
-    val lines = text.flatMap(t => t.split("\\s+"))
-    val words = lines.map(word => (word, 1))
-    val wordCount = words.reduceByKey(_ + _)
-    val result = wordCount.collectWithTaint()
-    result.foreach(r => println(r._1 + " " + r._2))
+    val movie_rating = in.taint(t => (1, 2, 3)).map(t => (t._2, t._3))
 
-    val words_taint = words.taint(t => {
-      t._1 match {
-        case "1" => (1, 2)
-        case "2" => (4, 4)
-        case _ => (0, 0)
-      }
-    })
-    val taintedResult = words_taint.collectWithTaint()
-    taintedResult.foreach(t => {
-      println(t._1 + " " + t._2)
-    })
-    val wordCountTaint = words_taint.reduceByKey(_ + _)
-    val result_taint = wordCountTaint.collectWithTaint()
-    result_taint.foreach(r => {
-      println(r._1 + " " + r._2)
-    })
+    val movie_count = movie_rating.reduceByKey(_ + _).zipWithTaint().collect().foreach(println)
 
     spark.stop()
   }
